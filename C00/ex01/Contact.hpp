1818765db93b4a30dab54c2ae237956853eaa454
dotacow <ykitaneh22@gmail.com>
2025-06-25 22:12:18 +0300

    Contact.hpp done

---
+++ C00/ex01/Contact.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 21:36:05 by yokitane          #+#    #+#             */
+/*   Updated: 2025/06/25 22:11:35 by yokitane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <string>
+
+class Contact
+{
+	private:
+		std::string	FirstName;
+		std::string	LastName;
+		std::string	NickName;
+		std::string	PhoneNumber;
+		std::string	DarkestSecret;
+	public:
+		// Canonical Form (rule of five)
+		Contact ();
+		Contact (std::string firstName, std::string lastName, std::string nickName,std::string phoneNumber, std::string darkestSecret);
+		Contact (const Contact &other);
+		Contact &operator=(const Contact &other);
+		~Contact ();
+		// Getters and Setters
+		std::string	ToString();
+		std::string	getFirstName();
+		std::string	getLastName();
+		std::string	getNickName();
+		std::string	getPhoneNumber();
+		std::string	getDarkestSecret();
+		void		setFirstName(std::string firstName);
+		void		setLastName(std::string lastName);
+		void		setNickName(std::string nickName);
+		void		setPhoneNumber(std::string phoneNumber);
+		void		setDarkestSecret(std::string darkestSecret);
+};
+#endif // CONTACT_HPP
