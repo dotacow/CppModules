f92f701be192e72dd25056cc66e855106dbcfd15
dotacow <ykitaneh22@gmail.com>
2025-07-03 18:50:41 +0300

    init ex02

---
+++ C01/ex02/Weapon.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 18:15:37 by yokitane          #+#    #+#             */
+/*   Updated: 2025/07/03 18:35:46 by yokitane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+# include <string>
+
+class Weapon
+{
+	private:
+		std::string _type;
+	public:
+		Weapon(const std::string type);
+		~Weapon();
+
+		const std::string &getType() const;
+		void setType(const std::string &type);
+};
+
+
+#endif //WEAPON_HPP
