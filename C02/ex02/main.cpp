329b046cc810f95b6f1ef55fb65b243e83d648af
dotacow <ykitaneh22@gmail.com>
2025-07-06 19:10:47 +0300

    more tests

--- C02/ex02/main.cpp
+++ C02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 05:18:42 by yokitane          #+#    #+#             */
-/*   Updated: 2025/07/06 19:05:57 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/07/06 19:10:22 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int main( void )
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	Fixed temp(5);
+	Fixed temp(-5);
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
-	std::cout << (temp = temp + 1 / 2) << std::endl;
+	std::cout << (temp = temp + 1.0f / 2.0f) << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
