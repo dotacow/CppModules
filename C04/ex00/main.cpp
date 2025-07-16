c8d8e23f8e2546594e66709df0562df7f57f8d35
dotacow <ykitaneh22@gmail.com>
2025-07-16 18:30:30 +0300

    more tests

--- C04/ex00/main.cpp
+++ C04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 17:07:39 by yokitane          #+#    #+#             */
-/*   Updated: 2025/07/16 18:21:40 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/07/16 18:29:43 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,14 @@ int main()
 	const Animal* meta = new Animal();
 	const Animal* dog = new Dog();
 	const Animal* cat = new Cat();
+	Animal			animal = *dog;
 	std::cout << dog->getType() << " " << std::endl;
 	std::cout << cat->getType() << " " << std::endl;
 	cat->makeSound();
 	dog->makeSound();
 	meta->makeSound();
-
+	animal.setType("bird");
+	std::cout << "Animal type after assignment: " << animal.getType() << std::endl;
 	delete meta;
 	delete cat;
 	delete dog;
