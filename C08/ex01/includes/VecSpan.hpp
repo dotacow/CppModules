6b12a0113f6c6bbff8503686269e99615798c257
dotacow <ykitaneh22@gmail.com>
2025-10-22 22:32:24 +0300

    testing out vector based approach

---
+++ C08/ex01/includes/VecSpan.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <vector>
+#include <stdexcept>
+
+class VecSpan
+{
+	private:
+		unsigned int		_maxSize;
+		std::vector<int>	_data;
+		long				_shortestSpan;
+		bool				_hasSpan;
+		bool				_sorted;
+		void				updateSpans();
+		VecSpan();
+	public:
+		// canonical form
+		VecSpan(unsigned int n);
+		VecSpan(const VecSpan &other);
+		VecSpan& operator=(const VecSpan &other);
+		~VecSpan();
+		// member methods
+		void				addNumber(int number);
+		long				shortestSpan();
+		long				longestSpan();
+		template<typename iterator>
+		void				addRange(iterator begin, iterator end)
+		{
+			while (begin != end)
+			{
+				try
+				{
+					addNumber(*begin);
+				}
+				catch (std::overflow_error &e)
+				{
+					throw;
+				}
+				++begin;
+			}
+		};
+
+		// exceptions
+		class NoSpanException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
